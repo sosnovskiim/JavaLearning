9316c148851e3df68780d5291c764e4ad85d664a
sosnovskiim <darkisosn2805@gmail.com>
2024-09-29 12:20:19 +0400

    Задача 1

--- src/main/java/org/knit/tasks/task1/Task1.java
+++ src/main/java/org/knit/tasks/task1/Task1.java
@@ -3,8 +3,8 @@ package org.knit.tasks.task1;
 public class Task1 {
     public static void main(String[] args) {
         StringBuilder result = new StringBuilder();
-        for (int i = 1; i < 100; i += 10) {
-            for (int j = i; j < i + 10; j++) {
+        for (int i = 1; i <= 99; i += 10) {
+            for (int j = i; j <= i + 9; j++) {
                 result.append(j);
                 if (j < i + 9) {
                     result.append(",");
